ffeec4f2d5cbc4fd155c6e9fc83a909dd4f35f41
JeansAthiwat <athiwat5146@gmail.com>
2025-07-04 19:16:14 +0900

    visualize and spin model

--- data/pyrender_viz_test.py
+++
@@ -1,7 +0,0 @@
-import trimesh
-import pyrender
-import numpy as np
-
-lod_3_mesh = trimesh.load("/home/jeans/win/aaaJAIST/resources/LOD_data_50/1/lod3.obj")
-scene = pyrender.Scene.from_trimesh_scene(lod_3_mesh)
-pyrender.Viewer(scene, use_raymond_lighting=True)
